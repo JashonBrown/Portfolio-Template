c17da191502c3cc032c87cc215edeffeedb61790
Kees Untamed <keest@protonmail.com>
2022-08-13 22:16:29 +1200

    Changed url format

--- src/components/ProjectWebGLCard.js
+++ src/components/ProjectWebGLCard.js
@@ -1,6 +1,6 @@
 import '../styles/ProjectWebGLCard.scss';
 
-// Format: <ProjectWebGLCard url='https://itch.io/embed-upload/project_id'/> where the url is the src url from the iframe on the "Embed Game" page on itch.io. 
+// Format: <ProjectWebGLCard url='https://itch.io/embed-upload/project_id'/> where the url is the direct link url from the "Embed Game" page on itch.io. 
 // Specify a non 16:9 aspect ratio using "aspectRatio = '16 / 9'"
 
 export default function ProjectWebGLCard(props) {
@@ -8,7 +8,7 @@ export default function ProjectWebGLCard(props) {
 
     return (
         <div className="project-web-gl-card" style={{aspectRatio: aspectRatio}}>
-                <iframe frameBorder="0" title="Game Embed Card" src={props.url + "?color=333333"} allowFullScreen="" width="100%" height="100%"/>
+                <iframe frameBorder="0" title="Game Embed Card" src={props.url} allowFullScreen="" width="100%" height="100%"/>
         </div>
     )
 }
