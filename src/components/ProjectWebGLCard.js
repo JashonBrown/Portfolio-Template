f464b1b5b692397e9c2efb2bebf58c20688d68f6
Kees Untamed <keest@protonmail.com>
2022-08-13 22:13:28 +1200

    Added ProjectWebGLCard

---
+++ src/components/ProjectWebGLCard.js
@@ -0,0 +1,14 @@
+import '../styles/ProjectWebGLCard.scss';
+
+// Format: <ProjectWebGLCard url='https://itch.io/embed-upload/project_id'/> where the url is the src url from the iframe on the "Embed Game" page on itch.io. 
+// Specify a non 16:9 aspect ratio using "aspectRatio = '16 / 9'"
+
+export default function ProjectWebGLCard(props) {
+    let aspectRatio = !props.aspectRatio ? '16 / 9' : props.aspectRatio
+
+    return (
+        <div className="project-web-gl-card" style={{aspectRatio: aspectRatio}}>
+                <iframe frameBorder="0" title="Game Embed Card" src={props.url + "?color=333333"} allowFullScreen="" width="100%" height="100%"/>
+        </div>
+    )
+}
