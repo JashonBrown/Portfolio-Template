0294a937aec558d20405b560d87746c2c30e55a8
Kees Untamed <keest@protonmail.com>
2022-08-16 10:35:40 +1200

    Few fixes for some of the components

--- src/components/ItchDownload.js
+++ src/components/ItchDownload.js
@@ -3,7 +3,7 @@ import '../styles/ItchDownload.scss';
 export default function ItchDownload(props) {
     return (
         <div className="itch-download">
-            <iframe frameborder="0" src={"https://itch.io/embed/" + props.src + "?bg_color=212529&amp;fg_color=eeeeee&amp;link_color=eeeeee&amp;border_color=212529"} width="100%" height="auto" title={props.title}><a href={props.download}>{props.title}</a></iframe>
+            <iframe frameborder="0" title={props.title} src={"https://itch.io/embed/" + props.src + "?border_width=0&bg_color=222222&fg_color=eeeeee&link_color=eeeeee&border_color=363636"} width="100%" height="165"><a href={props.download}>{props.title}</a></iframe>
         </div>
     )
 }
