738dcc6eeb5aaf58a049eb898224fac23840df36
Jashon <jashon.brown@gmail.com>
2022-12-11 15:09:46 +1300

    fixed allowfullscreen error on youtube card component

--- src/components/ProjectYoutubeEmbedCard.js
+++ src/components/ProjectYoutubeEmbedCard.js
@@ -3,7 +3,7 @@ import '../styles/ProjectYoutubeEmbedCard.scss';
 export default function ProjectYoutubeEmbedCard(props) {
     return (
         <div className="project-youtube-card" style={{aspectRatio: "16/9"}}>
-            <iframe width="100%" height="100%" src={"https://www.youtube-nocookie.com/embed/" + props.src} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="100%" height="100%" src={"https://www.youtube-nocookie.com/embed/" + props.src} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             {props.title ? <p style={{textAlign: "center", width: "100%"}}>{props.title}</p> : <></>}
         </div>
     )
