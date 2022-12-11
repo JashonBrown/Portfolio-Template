545fa9f2f5ce9919f6524be07b2a735df923b211
Jashon <jashon.brown@gmail.com>
2022-12-11 15:13:42 +1300

    fixed frameborder error

--- src/components/ItchDownload.js
+++ src/components/ItchDownload.js
@@ -3,7 +3,7 @@ import '../styles/ItchDownload.scss';
 export default function ItchDownload(props) {
     return (
         <div className="itch-download">
-            <iframe frameborder="0" title={props.title} src={"https://itch.io/embed/" + props.src + "?border_width=0&bg_color=222222&fg_color=eeeeee&link_color=eeeeee&border_color=363636"} width="100%" height="165"><a href={props.download}>{props.title}</a></iframe>
+            <iframe frameBorder="0" title={props.title} src={"https://itch.io/embed/" + props.src + "?border_width=0&bg_color=222222&fg_color=eeeeee&link_color=eeeeee&border_color=363636"} width="100%" height="165"><a href={props.download}>{props.title}</a></iframe>
         </div>
     )
 }
