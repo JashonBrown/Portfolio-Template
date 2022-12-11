4973de9bdd01b52233341032245894bab837c1ac
Jashon <jashon.brown@gmail.com>
2022-12-11 21:26:50 +1300

    added property to autoplay in for video cards

--- src/components/ProjectVideoCard.js
+++ src/components/ProjectVideoCard.js
@@ -1,9 +1,11 @@
 import '../styles/ProjectVideoCard.scss';
 
 export default function ProjectVideoCard(props) {
+    let autoplay = props.autoplay ?? true;
+
     return (
         <div className="project-video-card">
-            <video autoPlay controls src={props.src} type={props.type} />
+            <video autoPlay={autoplay} controls src={props.src} type={props.type} />
         </div>
     )
 }
